dce519fe92ceb146ddfbf87fa65a47f192cbbb98
doyou89.jung <doyou89.jung@samsung.com>
2019-04-09 15:44:34 +0900

    use path instead of url for django url

--- src/django_saml2_pro_auth/urls.py
+++ src/django_saml2_pro_auth/urls.py
@@ -1,9 +1,4 @@
-import re
-try:
-    from django.urls import re_path as url
-except ImportError:
-    from django.conf.urls import url
-    
+from django.urls import path, include
 from django.conf import settings
 
 from . import views
@@ -16,12 +11,12 @@ if SAML_ROUTE.strip()[-1] == '/':
 if SAML_ROUTE.strip()[0] == '/':
     SAML_ROUTE = SAML_ROUTE.lstrip('/')
 
-AUTH = r'^' + SAML_ROUTE + '/$'
-ACS = r'^' + SAML_ROUTE + '/acs/$'
-METADATA = r'^' + SAML_ROUTE + '/metadata/$'
+AUTH = SAML_ROUTE + '/'
+ACS = SAML_ROUTE + '/acs/'
+METADATA = SAML_ROUTE + '/metadata/'
 
 urlpatterns = [
-    url(AUTH, views.saml_login, name='saml2_auth'),
-    url(ACS, views.acs),
-    url(METADATA, views.metadata, name='metadata'),
+    path(AUTH, views.saml_login, name='saml2_auth'),
+    path(ACS, views.acs),
+    path(METADATA, views.metadata, name='metadata'),
 ]
